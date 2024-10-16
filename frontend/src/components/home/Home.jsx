bd21d3da7f777c28913a16878310c7a9e8c8d158
Bedknobs <noeliareysilva@gmail.com>
2024-10-16 18:10:48 +0200

    Started the Welcome part

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -20,6 +20,15 @@ export default function Home() {
       <div className="coverImage">
         <img src="../../../src/assets/homeImage.jpg" alt="Background" />
       </div>
+
+      <div className="welcomeText">
+        <p className="welcomeTitle">Bienvenido a Mi Portafolio Web</p>
+        <p className="welcomeSubtitle">
+          ¡Hola! Soy un desarrollador web en crecimiento, entusiasta y con
+          muchas ganas de aprender y mejorar, listo para enfrentar nuevos
+          desafíos y crear soluciones innovadoras.
+        </p>
+      </div>
     </>
   );
 }
