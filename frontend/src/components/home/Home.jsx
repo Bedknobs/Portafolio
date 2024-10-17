5637aba819d4d9e27390c218a88010d2750487eb
Bedknobs <noeliareysilva@gmail.com>
2024-10-17 22:37:14 +0200

    added style to the welcome text and added images and icons folders to assets

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -5,27 +5,30 @@ export default function Home() {
     <>
       <nav>
         <section className="navSection">
-          <img src="../../../src/assets/rabbitIcon.svg" alt="Rabbit Icon" />
+          <img
+            src="../../../src/assets/icons/rabbitIcon.svg"
+            alt="Rabbit Icon"
+          />
           <p>NOELIA REY</p>
         </section>
         <section className="navSection">
           <a href="">INICIO</a>
           <a href="">SOBRE MI</a>
-          <a href="">SKILLS</a>
           <a href="">PROYECTOS</a>
+          <a href="">PEQUEÑOS DESAFIOS</a>
           <a href="">CONTACTO</a>
         </section>
       </nav>
 
       <div className="coverImage">
-        <img src="../../../src/assets/homeImage.jpg" alt="Background" />
+        <img src="../../../src/assets/images/homeImage.jpg" alt="Background" />
       </div>
 
       <div className="welcomeText">
         <p className="welcomeTitle">Bienvenido a Mi Portafolio Web</p>
         <p className="welcomeSubtitle">
-          ¡Hola! Soy un desarrollador web en crecimiento, entusiasta y con
-          muchas ganas de aprender y mejorar, listo para enfrentar nuevos
+          ¡Hola! Soy una desarrolladora web en crecimiento, entusiasta y con
+          muchas ganas de aprender y mejorar, lista para enfrentar nuevos
           desafíos y crear soluciones innovadoras.
         </p>
       </div>
